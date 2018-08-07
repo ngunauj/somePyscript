005e947b42c5368912e9ad30402c0f3fd627da2b
guanjun <gaunjunace@foxmail.com>
2018-08-07 10:21:16 +0800

    fix bug

--- make_video.py
+++ make_video.py
@@ -5,7 +5,7 @@ import cv2
 img_path = gb.glob("out2.4/*.jpg")
  
 videoWriter = cv2.VideoWriter('/home/guan/Desktop/out.mp4', cv2.VideoWriter_fourcc(*'MJPG'), 25, (640, 480))
-
+img_path.sort()
 for path in img_path:
     img  = cv2.imread(path) 
     img = cv2.resize(img, (640, 480))
